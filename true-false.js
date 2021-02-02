7a8d34fc0f92049db8259d8fb2f2757f342da490
Pulok Chowdhury <riopulok@gmail.com>
2021-02-02 21:41:55 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,17 @@
+const age = 0;
+//if number is 0 it will be declared as false otherwise true
+const name = "";
+//if string is empty "" it will be declared as false otherwise true
+let name;
+let name = null;
+let name = NaN;
+//if variable is (undefined && null && NaN) it will be declared as false otherwise true
+let name = {};
+let name = [];
+//empty array & object is true cz they assigned their memory spaces 
+if(age){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
